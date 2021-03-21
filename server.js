ce44afc4647537660ea00bc038c3ace46c00c954
Carl <cfuer12@yahoo.com>
2021-03-21 09:54:28 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(compression());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
-  useNewUrlParser: true, 
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
